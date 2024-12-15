0b7b26a8e53426adc2ea46f5cac41c78487ee1e3
Libra420T <ryantf420@gmail.com>
2024-12-15 11:57:44 -0500

    Zuma: Lineage --> PixelStar

--- lineage_common.mk
+++ lineage_common.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-pixelstar
 
 # AiAi Config
 PRODUCT_COPY_FILES += \
